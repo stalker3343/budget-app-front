a7b5fa6e7c9ad5cda57b0269c838c51d481cdd25
Artem Byastic <stalker3343@gmail.com>
2024-10-04 23:31:10 +0300

    remove old api client use

--- consts.ts
+++ consts.ts
@@ -1,11 +1,4 @@
-import { TransactionsNestedListParamsEnum } from "~/api/generated/budget/data-contracts";
-
 export const TRANSACTION_TYPES = {
   INCOME: "Income",
   EXPENSE: "Expense",
-};
-
-export const TRANSACTION_LINK_FIELDS = {
-  ACCOUNTS: TransactionsNestedListParamsEnum.Ccfz0N80U7Lsg1F,
-  CATEGORIES: TransactionsNestedListParamsEnum.Ctjbax3Dzk44Clh,
 };
